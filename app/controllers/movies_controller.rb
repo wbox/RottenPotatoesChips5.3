7595b61632103980f88e5739c7cf4a50bc670522
Samuel Ribas <sribas@gmail.com>
2022-10-03 15:49:45 -0400

    fix css movie_title variable control name

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -35,7 +35,7 @@ class MoviesController < ApplicationController
         @movie_title = 'hilite bg-warning'
         @movies = Movie.with_ratings(@ratings_to_show).order(:title)
       else
-        @hilite_bg = ''
+        @@movie_title = ''
       end
 
       if params[:release_date_title].present?
